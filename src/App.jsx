03baa80840cfeda47add4deddf67cde22e4cd17d
hagbazz <hagbazzinfo001@gmail.com>
2025-01-29 14:52:07 +0100

    footer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+  import Footer from './components/footer'
+  import Navbar from '../src/components/Nav'
+ 
+function App() {
+ 
+  return (
+    <> 
+    <Navbar/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+ import '../assets/css/Nav.css';
+  function Nav( ) {
+    return (
+        < >
+          <nav>
+            <section>
+                <div>
+                    <img src="../assets/images/chart icon.svg" alt=""  />
+                    <span>Analytics integration</span>
+                </div>
+                <div>
+                <img src="../assets/images/notification.svg" alt=""  />
+                    <img src="../assets/images/setting icon.svg" alt="" />
+                </div>
+            </section>
+            </nav>  
+        </ >
+    );
+}
+
+export default Nav;
